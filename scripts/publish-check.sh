e6c1f9fb3a1b782bb0cf8e3e81a4059ae617c030
d-kimsuon <dev.kimuson@gmail.com>
2025-11-07 01:04:24 +0900

    chore: add pnpm audit check before publish

--- scripts/publish-check.sh
+++ scripts/publish-check.sh
@@ -5,6 +5,7 @@ set -ueo pipefail
 pnpm typecheck
 pnpm lint
 pnpm build
+pnpm audit
 
 output_file=$(npm pack --json | jq -r '.[].filename')
 trap 'if [ -n "$output_file" ]; then rm -f ./$output_file; fi' EXIT
